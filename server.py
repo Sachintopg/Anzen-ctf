b3c73d63166e92060ff291be0e5089a782aef185
Sachintopg <136297648+Sachintopg@users.noreply.github.com>
2024-10-22 18:46:06 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -8,11 +8,16 @@ def validate_payload():
     data = request.json
     
     # Check for the exact match of '-alert(xss)-'
-    correct_payload = '-alert(1)-'
+    correct_payload = '-alert(xss)-'
     
     if data and data['payload'].strip() == correct_payload:
-        flag = "Anzen CTF{alert}"
-        return jsonify({"valid": True, "flag": flag})
+        try:
+            # Execute the payload as JavaScript code
+            eval(data['payload'])
+            flag = "Anzen CTF{alert}"
+            return jsonify({"valid": True, "flag": flag})
+        except Exception as e:
+            return jsonify({"valid": False, "error": str(e)})
     else:
         return jsonify({"valid": False})
 
